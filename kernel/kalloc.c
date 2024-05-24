12ca57ef4fa5e40805298757db28fd1138616eaf
FPOOOOOOO <1284946208@qq.com>
2024-05-24 17:38:00 +0800

    LAB8 memory allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,12 +21,17 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];  //FPO LAB8
 
 void
-kinit()
+kinit() 
 {
-  initlock(&kmem.lock, "kmem");
+  //initlock(&kmem.lock, "kmem"); // FPO LAB8
+  for (int i = 0; i < NCPU; i++) {
+    char name[9] = {0};
+    snprintf(name, 8, "kmem-%d", i);
+    initlock(&kmem[i].lock, name);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -51,16 +56,48 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
+  push_off();// FPO LAB8
+  int cpu = cpuid();
+
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&kmem[cpu].lock);// FPO LAB8
+  r->next = kmem[cpu].freelist;
+  kmem[cpu].freelist = r;
+  release(&kmem[cpu].lock);
+
+  pop_off();// FPO LAB8
 }
+
+// Try steal a free physical memory page from another core
+// interrupt should already be turned off
+// return NULL if not found free page FPO LAB8
+void *
+ksteal(int cpu) {
+  struct run *r;
+  for (int i = 1; i < NCPU; i++) {
+    // 从右边的第一个邻居开始偷
+    int next_cpu = (cpu + i) % NCPU;
+    // --- critical session ---
+    acquire(&kmem[next_cpu].lock);
+    r = kmem[next_cpu].freelist;
+    if (r) {
+      // steal one page
+      kmem[next_cpu].freelist = r->next;
+    }
+    release(&kmem[next_cpu].lock);
+    // --- end of critical session ---
+    if (r) {
+      break;
+    }
+  }
+  // 有可能返回NULL, 如果邻居也都没有空余页的话
+  return r;
+}
+
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
@@ -70,13 +107,73 @@ kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();//FPO LAB8
+  int cpu = cpuid();//FPO LAB8
+  
+  acquire(&kmem[cpu].lock);//FPO LAB8
+  r = kmem[cpu].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[cpu].freelist = r->next;
+  release(&kmem[cpu].lock);
+
+  if(r == 0){//FPO LAB8
+    r = ksteal(cpu);
+  }
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
+  
+  pop_off();//FPO LAB8
   return (void*)r;
 }
+
+// void *  //取走一半
+// kalloc(void)
+// {
+//   struct run *r;
+//   push_off();
+//   int id = cpuid();
+//   acquire(&kmem[id].lock);
+//   r = kmem[id].freelist;
+//   if(r) {
+//     kmem[id].freelist = r->next;
+//     release(&kmem[id].lock);
+//   } else {
+//     release(&kmem[id].lock);
+//     for (int i = 0; i < NCPU; ++i) {
+//       if (i == id) {
+//         continue;
+//       }
+//       if (i < id) {
+//         acquire(&kmem[i].lock);
+//         acquire(&kmem[id].lock);
+//       } else {
+//         acquire(&kmem[id].lock);
+//         acquire(&kmem[i].lock);
+//       }
+//       struct run *slow = kmem[i].freelist; 
+//       struct run *fast = slow;
+//       while (fast && fast->next) {
+//         fast = fast->next->next;
+//         slow = slow->next;
+//       }
+//       if (slow) {
+//         kmem[id].freelist = kmem[i].freelist;
+//         kmem[i].freelist = slow->next;
+//         slow->next = 0;
+//         r = kmem[id].freelist;
+//         kmem[id].freelist = r->next;
+//         release(&kmem[id].lock);
+//         release(&kmem[i].lock);
+//         break;
+//       } 
+//       release(&kmem[id].lock);
+//       release(&kmem[i].lock); 
+//     }
+//   }
+
+//   if(r)
+//     memset((char*)r, 5, PGSIZE); // fill with junk
+//   pop_off();
+//   return (void*)r;
+// }
