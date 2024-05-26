bb7ff89970ffb7175adf02df6d8f723f99d65940
FPOOOOOOO <1284946208@qq.com>
2024-05-26 13:10:03 +0800

    LAB10

--- user/user.h
+++ user/user.h
@@ -22,6 +22,9 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+void* mmap(void *addr, int length, int prot, int flags,   //FPO LAB10
+           int fd, int offset);
+int munmap(void *addr, int length);
 
 // ulib.c
 int stat(const char*, struct stat*);
